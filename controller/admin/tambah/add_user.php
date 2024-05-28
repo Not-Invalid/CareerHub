bc5fef994bd15cccd0412501dc11cf1e6895cdd1
RaffDev31 <raffli4000@hotmail.com>
2024-05-29 06:53:24 +0700

    feat: Add automatic password generation feature

--- controller/admin/tambah/add_user.php
+++ controller/admin/tambah/add_user.php
@@ -1,21 +1,37 @@
 <?php
-include "../../../config/koneksi.php";
+include '../../../config/koneksi.php';
+
 if (isset($_POST['tambah'])) {
-    $id_user = $_POST['id_user'];
-    $nisn = $_POST['nisn'];
-    $password = $_POST['password'];
-    $id_role = $_POST['id_role'];
+    $nisns = $_POST['nisn'];
+    $roles = $_POST['id_role'];
 
-    $query = mysqli_query($koneksi, "INSERT INTO users (id_user, nisn, password, id_role) 
-    VALUES ('$id_user','$nisn','$password','$id_role')");
-    
+    function generateRandomPassword($length = 8) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $password = '';
+        for ($i = 0; $i < $length; $i++) {
+            $password .= $characters[rand(0, strlen($characters) - 1)];
+        }
+        return $password;
+    }
 
-if ($query) {
-    header('location:../../../views/admin/user.php?status=Sukses');
-} else {
-    header('location:../../../views/admin/user.php?status=gagal');
-}
-} else {
-    die ("Akses dilarang...");
+    for ($i = 0; $i < count($nisns); $i++) {
+        $nisn = $nisns[$i];
+        $role = $roles[$i];
+        $password = generateRandomPassword(); 
+
+        $query = "INSERT INTO users (nisn, password, id_role) VALUES ('$nisn', '$password', '$role')";
+        $result = mysqli_query($koneksi, $query);
+
+        if (!$result) {
+
+            echo "Error: " . mysqli_error($koneksi);
+            exit;
+        }
+
+        echo "User with NISN: $nisn added successfully. Generated password: $password <br>";
+    }
+
+    header('Location: ../../../views/admin/user.php?status=Sukses');
+    exit;
 }
 ?>
