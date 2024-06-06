f8b970999578ec4d9498a844b9d118d2e6c0a73a
RaffDev31 <raffli4000@hotmail.com>
2024-06-06 10:45:49 +0700

    refactor: change footer layout

--- layout/footer.php
+++ layout/footer.php
@@ -21,7 +21,7 @@
             <div class="copyright">
                 <div class="row">
                     <div class="col-md-6 text-center text-md-center mb-3 mb-md-0">
-                        &copy; <a class="" href="#">CareerHub</a>, All Right Reserved.
+                        &copy; <span class="">CareerHub</span>, All Right Reserved.
 
                         Designed By <span class="copyright-text">Kelompok 7</span>
                     </div>
